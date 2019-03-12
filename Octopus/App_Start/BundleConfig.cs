98a5a383d6f98247d1f665301edb1e3664c89fdd
Martín Guereta <guereta.martin@gmail.com>
2019-03-12 20:28:09 -0300

    Cambio

--- Octopus/App_Start/BundleConfig.cs
+++ Octopus/App_Start/BundleConfig.cs
@@ -3,7 +3,7 @@ using System.Web.Optimization;
 
 namespace Octopus
 {
-    public class BundleConfig
+    public class BundleConfig //coment
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
