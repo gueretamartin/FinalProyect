ffaaf53d5602b9857de761917326415d392a8b19
Martín Guereta <guereta.martin@gmail.com>
2019-03-31 11:05:41 -0300

    Agrego ABM de Empleados

--- Octopus/Controllers/EmpleadosController.cs
+++ Octopus/Controllers/EmpleadosController.cs
@@ -14,10 +14,28 @@ namespace Octopus.Controllers
     {
         private OctopusEntities db = new OctopusEntities();
 
+
+
         // GET: /Empleados/
         public ActionResult Index()
         {
             var empleados = db.EMPLEADOS.Include(e => e.SUCURSALES);
+            return View(db.EMPLEADOS.ToList());
+        }
+
+        #region DesarrolloMartin
+        public ActionResult List(string searchEmpleado)
+        {
+             var empleados = from c in db.EMPLEADOS
+                           select c;
+
+           
+            if (!String.IsNullOrEmpty(searchEmpleado))
+            {
+                empleados = empleados.Where(c => c.EMP_NOMBRE.Contains(searchEmpleado)
+                    || c.EMP_APELLIDO.Contains(searchEmpleado)
+                    || c.EMP_DNI.Contains(searchEmpleado));
+            }
             return View(empleados.ToList());
         }
 
@@ -36,6 +54,11 @@ namespace Octopus.Controllers
             return View(empleados);
         }
 
+       
+
+        #endregion
+
+
         // GET: /Empleados/Create
         public ActionResult Create()
         {
@@ -43,71 +66,105 @@ namespace Octopus.Controllers
             return View();
         }
 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EMPLEADOS empleados)
+        {
+            //CHEQUEA SI EL DOCUMENTO YA FUE CARGADO
+           
+
+            if (ModelState.IsValid)
+            {
+             //   var empleado = db.EMPLEADOS.Single(u => u.EMP_ID == empleados.EMP_ID);
+                empleados.EMP_APELLIDO_NOMBRE = empleados.EMP_APELLIDO + " " + empleados.EMP_NOMBRE;
+                //CleanClient(clientes);
+                db.EMPLEADOS.Add(empleados);
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+
+            return View(empleados);
+        }
+
+
+
         // POST: /Empleados/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="EMP_ID,SUC_ID,EMP_CODPOSTAL,EMP_NOMBRE,EMP_APELLIDO,EMP_EMAIL,EMP_USER,EMP_PASSWORD,EMP_TIPO,EMP_DIRECCION,EMP_TELEFONO,EMP_DNI,EMP_APELLIDO_NOMBRE")] EMPLEADOS empleados)
-        {
-            if (ModelState.IsValid)
-            {
-                db.EMPLEADOS.Add(empleados);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include="EMP_ID,SUC_ID,EMP_CODPOSTAL,EMP_NOMBRE,EMP_APELLIDO,EMP_EMAIL,EMP_USER,EMP_PASSWORD,EMP_TIPO,EMP_DIRECCION,EMP_TELEFONO,EMP_DNI,EMP_APELLIDO_NOMBRE")] EMPLEADOS empleados)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.EMPLEADOS.Add(empleados);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
 
-            ViewBag.SUC_ID = new SelectList(db.SUCURSALES, "SUC_ID", "SUC_DESCRIP", empleados.SUC_ID);
-            return View(empleados);
+        //    ViewBag.SUC_ID = new SelectList(db.SUCURSALES, "SUC_ID", "SUC_DESCRIP", empleados.SUC_ID);
+        //    return View(empleados);
+        //}
+
+        public ActionResult Edit(int EMP_ID)
+        {
+            try
+            {
+                ViewBag.SUC_ID = new SelectList(db.SUCURSALES, "SUC_ID", "SUC_DESCRIP", db.EMPLEADOS.SingleOrDefault(e => e.EMP_ID == EMP_ID).SUC_ID);
+                EMPLEADOS empleado = db.EMPLEADOS.SingleOrDefault(e => e.EMP_ID == EMP_ID);
+                return View(empleado);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
-        // GET: /Empleados/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EMPLEADOS empleados = db.EMPLEADOS.Find(id);
-            if (empleados == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.SUC_ID = new SelectList(db.SUCURSALES, "SUC_ID", "SUC_DESCRIP", empleados.SUC_ID);
-            return View(empleados);
-        }
+        //Edit an Employee 
 
-        // POST: /Empleados/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="EMP_ID,SUC_ID,EMP_CODPOSTAL,EMP_NOMBRE,EMP_APELLIDO,EMP_EMAIL,EMP_USER,EMP_PASSWORD,EMP_TIPO,EMP_DIRECCION,EMP_TELEFONO,EMP_DNI,EMP_APELLIDO_NOMBRE")] EMPLEADOS empleados)
+        public ActionResult Edit(EMPLEADOS empleados)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(empleados).State = EntityState.Modified;
+                var empleado = db.EMPLEADOS.Single(u => u.EMP_ID == empleados.EMP_ID);
+                empleado.EMP_APELLIDO_NOMBRE = empleados.EMP_APELLIDO + " " + empleados.EMP_NOMBRE;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("List");
             }
             ViewBag.SUC_ID = new SelectList(db.SUCURSALES, "SUC_ID", "SUC_DESCRIP", empleados.SUC_ID);
             return View(empleados);
         }
 
-        // GET: /Empleados/Delete/5
-        public ActionResult Delete(int? id)
+
+        // POST: /Vehiculos/Delete/5
+        
+        public ActionResult Delete(int emp_id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EMPLEADOS empleados = db.EMPLEADOS.Find(id);
-            if (empleados == null)
-            {
-                return HttpNotFound();
-            }
-            return View(empleados);
+            EMPLEADOS empleado = db.EMPLEADOS.Find(emp_id);
+            db.EMPLEADOS.Remove(empleado);
+            db.SaveChanges();
+            return RedirectToAction("List");
         }
+
+ 
+        //// GET: /Empleados/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    EMPLEADOS empleados = db.EMPLEADOS.Find(id);
+        //    if (empleados == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(empleados);
+        //}
 
         // POST: /Empleados/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -128,5 +185,30 @@ namespace Octopus.Controllers
             }
             base.Dispose(disposing);
         }
+
+        // Menú en el detalle 
+
+        [HttpPost]
+        public ActionResult Acciones(int emp_id, string Action)
+        {
+            if (Action == "Edit")
+            {
+                return RedirectToAction("Edit", new { emp_id = emp_id });
+            }
+            else if (Action == "Delete")
+            {
+                return Delete(emp_id);
+            }
+            else
+            {
+                return View("List");
+            }
+        }
+    
+    
+    
+    
+    
+    
     }
 }
